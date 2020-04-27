ccf1ec9d6a5ac5a097035f2f54f78535a8b4ca2b
liuhongzhi64 <1069223278@qq.com>
2020-04-27 22:08:17 +0800

    添加了地图的中间显示位置和一个搜索

--- applet-management-system/src/pages/Index/IndexMap/indexMap.vue
+++ applet-management-system/src/pages/Index/IndexMap/indexMap.vue
@@ -19,15 +19,21 @@ export default {
   methods: {
     map() {
       let map = new window.BMap.Map(this.$refs.allmap); // 创建Map实例
-      map.centerAndZoom(new window.BMap.Point(116.404, 39.915), 11); // 初始化地图,设置中心点坐标和地图级别
+      // map.centerAndZoom(new window.BMap.Point(116.404, 39.915), 11); // 初始化地图,设置中心点坐标和地图级别
+      map.centerAndZoom("成都", 11); // 初始化地图,设置中心点坐标和地图级别
       map.addControl(
         new window.BMap.MapTypeControl({
           // 添加地图类型控件
           mapTypes: [window.BMAP_NORMAL_MAP, window.BMAP_HYBRID_MAP]
         })
       );
-      map.setCurrentCity("北京"); // 设置地图显示的城市 此项是必须设置的
+      // map.setCurrentCity("北京"); // 设置地图显示的城市 此项是必须设置的
+      map.setCurrentCity("成都"); // 设置地图显示的城市 此项是必须设置的
       map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
+      var local = new window.BMap.LocalSearch(map, {
+        renderOptions: { map: map , panel: "results"}
+      });
+      local.search("天府广场");
     }
   },
   mounted() {
@@ -40,16 +46,15 @@ export default {
 .indexMap {
   width: 100%;
   height: 100%;
-
   .map {
     width: 90%;
     height: 90%;
     background-color: #fff;
     margin: 0 auto;
-    padding-top:2%;
-    .maps{
-        width: 100%;
-        height: 100%;
+    padding-top: 2%;
+    .maps {
+      width: 100%;
+      height: 100%;
     }
     #allmap {
       margin: 0 auto;
